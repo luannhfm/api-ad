54b41cd14beac2c2b5fc37e9cdb1def5e382f9ac
Ubuntu <ubuntu@ip-172-31-21-124.ec2.internal>
2025-05-04 16:44:16 +0000

    fix

--- src/modules/auth/checkUser.route.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { checkUserController } from './checkUser.controller';
-
-export async function authRoutes(fastify: FastifyInstance) {
-  fastify.post('/auth', checkUserController);
-}
